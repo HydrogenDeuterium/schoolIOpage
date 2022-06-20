a72fbe1a4027656e0793f839b8b852703704cc02
Deuterium <Deu-7GZ@deuterium.wiki>
2022-06-21 00:35:30 +0800

    update

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('navigate.html')
+
+
+@app.route('/leave')
+def leave():  # put
+    return render_template('lixiao.html')
+
+
+@app.route('/into')
+def into():
+    return render_template('ruxiao.html')
 
 
 if __name__ == '__main__':
